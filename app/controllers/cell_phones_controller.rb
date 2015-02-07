a0042d5f4e3276f6837074f9eb5dabaf06acab4a
Chase Southard <chase.southard@gmail.com>
2015-02-06 22:16:20 -0500

    add some comments and more method stubs

--- app/controllers/cell_phones_controller.rb
+++ app/controllers/cell_phones_controller.rb
@@ -1,10 +1,12 @@
 class CellPhonesController < ApplicationController
-  before_action :set_cell_phone, except: [:new, :create, :validate]
+  before_action :set_cell_phone, except: [:new, :create]
 
+  # GET /cell_phones/new
   def new
     @cell_phone = CellPhone.new
   end
 
+  # POST /cell_phones
   def create
     @cell_phone = CellPhone.new(cell_phone_params)
     if @cell_phone.save
@@ -17,15 +19,25 @@ class CellPhonesController < ApplicationController
     end
   end
 
+  # GET /cell_phones/1
   def show
   end
 
+  # GET /cell_phones/1/edit
   def edit
   end
 
+  # PUT /cell_phones/1
   def update
+    if @cell_phone.update(cell_phone_params)
+      flash[:notice] = 'Your phone has been updated.'
+    else
+      flash[:warning] = 'We could not update your phone.'
+    end
+    redirect_to cell_phone_path(@cell_phone)
   end
 
+  # DELETE /cell_phones/1
   def destroy
   end
 
@@ -40,7 +52,14 @@ class CellPhonesController < ApplicationController
     redirect_to edit_cell_phone_path(@cell_phone)
   end
 
+  # POST /cell_phones/1/reset
   def reset
+    if @cell_phone.reset!
+      flash[:notice] = 'We\'re resetting your authorization code.'
+    else
+      flash[:warning] = 'We could not reset your authorization code.'
+    end
+    redirect_to edit_cell_phone_path(@cell_phone)
   end
 
   private
