c1c846dab573b98f55434c8146b21dc9e9be4e6e
Chase Southard <chase.southard@gmail.com>
2015-02-12 21:38:17 -0500

    basic auth with sorcery

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
+  resources :user_sessions
+  get 'login' => 'user_sessions#new', as: :login
+  post 'logout' => 'user_sessions#destroy', as: :logout
+
+  resources :users
+
   resources :cell_phones, except: [:index] do
     member do
       post :verify
       get  :reset
     end
   end
+
+  root to: 'users#index'
 end
---
+++ spec/factories/users.rb
@@ -0,0 +1,6 @@
+FactoryGirl.define do
+  factory :user do
+    email "MyString"
+    crypted_password "MyString"
+  end
+end
