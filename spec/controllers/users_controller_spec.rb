1ca1938afafb56809266aab87484e1fcb953029a
Chase Southard <chase.southard@gmail.com>
2015-02-15 18:55:41 -0500

    fix rubocop violations

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -24,7 +24,7 @@ RSpec.describe UsersController, type: :controller do
 
     describe 'GET show' do
       it 'redirects the user to sign in' do
-        get :show, {id: user.to_param}
+        get :show, id: user.to_param
         expect(response).to redirect_to(login_path)
       end
     end
@@ -40,30 +40,30 @@ RSpec.describe UsersController, type: :controller do
       describe 'with valid params' do
         it 'creates a new User' do
           expect {
-            post :create, {user: valid_attributes}
+            post :create, user: valid_attributes
           }.to change(User, :count).by(1)
         end
 
         it 'assigns a newly created user as @user' do
-          post :create, {user: valid_attributes}
+          post :create, user: valid_attributes
           expect(assigns(:user)).to be_a(User)
           expect(assigns(:user)).to be_persisted
         end
 
         it 'redirects to the created user' do
-          post :create, {user: valid_attributes}
+          post :create, user: valid_attributes
           expect(response).to redirect_to(User.last)
         end
       end
 
       describe 'with invalid params' do
         it 'assigns a newly created but unsaved user as @user' do
-          post :create, {user: invalid_attributes}
+          post :create, user: invalid_attributes
           expect(assigns(:user)).to be_a_new(User)
         end
 
         it 're-renders the new template' do
-          post :create, {user: invalid_attributes}
+          post :create, user: invalid_attributes
           expect(response).to render_template('new')
         end
       end
@@ -86,14 +86,14 @@ RSpec.describe UsersController, type: :controller do
 
     describe 'GET show' do
       it 'assigns the requested user as @user' do
-        get :show, {id: user.to_param}
+        get :show, id: user.to_param
         expect(assigns(:user)).to eq(user)
       end
     end
 
     describe 'GET edit' do
       it 'assigns the requested user as @user' do
-        get :edit, {id: user.to_param}
+        get :edit, id: user.to_param
         expect(assigns(:user)).to eq(user)
       end
     end
@@ -105,29 +105,29 @@ RSpec.describe UsersController, type: :controller do
         }
 
         it 'updates the requested user' do
-          put :update, {id: user.to_param, user: new_attributes}
+          put :update, id: user.to_param, user: new_attributes
           expect(user.reload.email).to eq(new_attributes[:email])
         end
 
         it 'assigns the requested user as @user' do
-          put :update, {id: user.to_param, user: new_attributes}
+          put :update, id: user.to_param, user: new_attributes
           expect(assigns(:user)).to eq(user)
         end
 
         it 'redirects to the user' do
-          put :update, {id: user.to_param, user: new_attributes}
+          put :update, id: user.to_param, user: new_attributes
           expect(response).to redirect_to(user)
         end
       end
 
       describe 'with invalid params' do
         it 'assigns the user as @user' do
-          put :update, {id: user.to_param, user: invalid_attributes}
+          put :update, id: user.to_param, user: invalid_attributes
           expect(assigns(:user)).to eq(user)
         end
 
         it 're-renders the edit template' do
-          put :update, {id: user.to_param, user: invalid_attributes}
+          put :update, id: user.to_param, user: invalid_attributes
           expect(response).to render_template('edit')
         end
       end
@@ -136,12 +136,12 @@ RSpec.describe UsersController, type: :controller do
     describe 'DELETE destroy' do
       it 'destroys the requested user' do
         expect {
-          delete :destroy, {id: user.to_param}
+          delete :destroy, id: user.to_param
         }.to change(User, :count).by(-1)
       end
 
       it 'redirects to the users list' do
-        delete :destroy, {id: user.to_param}
+        delete :destroy, id: user.to_param
         expect(response).to redirect_to(users_url)
       end
     end
