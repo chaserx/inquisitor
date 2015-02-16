2c22f5f2efa3f2e6399477b57673a9430ec6367c
Chase Southard <chase.southard@gmail.com>
2015-02-15 21:52:45 -0500

    add foundation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,5 +12,7 @@
 //
 //= require jquery
 //= require jquery_ujs
-//= require materialize
+//= require mailcheck
+//= require modernizr/modernizr
+//= require foundation/js/foundation.min
 //= require_tree .
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,9 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
-  # This should return the minimal set of attributes required to create a valid
-  # User. As you add validations to User, be sure to
-  # adjust the attributes here as well.
   let(:valid_attributes) {
     attributes_for(:user)
   }
