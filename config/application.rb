d9195fbe03abf157cdec7f2898eed849783df3b9
Chase Southard <chase.southard@gmail.com>
2015-02-18 09:05:08 -0500

    load lib dir

--- config/application.rb
+++ config/application.rb
@@ -33,5 +33,6 @@ module Inquisitor
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
     config.assets.paths << Rails.root.join('vendor', 'assets', 'components')
+    config.autoload_paths << Rails.root.join('lib')
   end
 end
