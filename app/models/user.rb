44ed6693a781b22380485d335716dbc1f99e29ee
Chase Southard <chase.southard@gmail.com>
2015-02-21 21:18:25 -0500

    timezone

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,13 @@ class User < ActiveRecord::Base
 
   has_one :mobile_phone
 
+  store_accessor :settings, :time_zone
+
   validates :password, length: { minimum: 3 }, if: :crypted_password_changed?
   validates :password, confirmation: true, if: :crypted_password_changed?
   validates :password_confirmation, presence: true, if: :crypted_password_changed?
   validates :email, uniqueness: true, format: { with: /.+@.+\..+/i }, if: :email_changed?
+  validates_inclusion_of :time_zone,
+                         in: ActiveSupport::TimeZone.zones_map(&:name).keys,
+                         message: 'is not a valid Time Zone'
 end
