80213aca3c3f9238721c9aa655498988e6986c99
Chase Southard <chase.southard@gmail.com>
2015-03-01 16:07:33 -0500

    appease rubocop

--- spec/factories/mobile_phones.rb
+++ spec/factories/mobile_phones.rb
@@ -2,7 +2,7 @@ require 'securerandom'
 FactoryGirl.define do
   factory :mobile_phone do
     user
-    number { "+1859555" + ('%04d' % SecureRandom.random_number(9999)) }
+    number { '+1859555' + ('%04d' % SecureRandom.random_number(9999)) }
 
     trait :verified do
       verified true
