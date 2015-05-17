cf4b8075f1a7f286b00a7e916756a02c3975a2fc
Chase Southard <chase.southard@gmail.com>
2015-05-16 22:26:38 -0400

    add some pending tests for decipher command method

--- spec/models/sms_spec.rb
+++ spec/models/sms_spec.rb
@@ -6,4 +6,20 @@ RSpec.describe SMS, type: :model do
       expect(subject).to have(1).errors_on(:mobile_phone)
     end
   end
+
+  describe '#decipher_command with start message' do
+    pending "not there yet"
+  end
+
+  describe '#decipher_command with stop message' do
+    pending "not there yet"
+  end
+
+  describe '#decipher_command with help message' do
+    pending "not there yet"
+  end
+
+  describe '#decipher_command with an unrecognized message' do
+    pending "not there yet"
+  end
 end
