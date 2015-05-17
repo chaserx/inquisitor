1e71114e7fd04ac039e9d5da58518fc64a115fda
Chase Southard <chase.southard@gmail.com>
2015-05-16 22:13:03 -0400

    add validation specs

--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'a new answer' do
+    it 'fails validation with no body' do
+      expect(subject).to have(1).errors_on(:body)
+    end
+  end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'a new question' do
+    it 'fails validation with no body' do
+      puts subject.errors.inspect
+      expect(subject).to have(2).errors_on(:body)
+    end
+
+    it 'fails validation with no user' do
+      expect(subject).to have(1).errors_on(:user_id)
+    end
+  end
+
+  describe 'an existing question awaiting an answer' do
+    pending "not there yet"
+  end
+
+  describe 'an existing answered question' do
+    pending "not there yet"
+  end
 end
